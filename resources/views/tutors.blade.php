96cfeeb5421de1cc59a7948880238a93f431a9e6
Fanan Dala <fanan.dala@yahoo.com>
2019-02-03 18:25:40 +0100

    completed tutor frontend

--- resources/views/tutors.blade.php
+++ resources/views/tutors.blade.php
@@ -30,54 +30,27 @@
 				<div class="fh5co-blog animate-box">
 					<a href="#"><img class="img-responsive" src="{{ asset('master/images/stock/tutor1.jpeg') }}" alt=""></a>
 					<div class="blog-text">
-						<h3><a href=""#>45 Minimal Workspace Rooms for Web Savvys</a></h3>
-						<span class="posted_on">Nov. 15th</span>
-						<span class="comment"><a href="">21<i class="icon-speech-bubble"></i></a></span>
-						<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-						<a href="#" class="btn btn-primary">Read More</a>
+						<h3><a href=""#>John Doe</a></h3>
+						<span class="posted_on">Tutor since Nov. 15th, 2018</span>
+						<p class="comment">Tutorials Done: <a class="pull-right" href="">21<i class="icon-speech-bubble"></i></a></p>
+						<p>Locations Available: Lekki, VGC, Ajah</p>
+						<a href="#" class="btn btn-primary">Request</a>
 					</div> 
 				</div>
 			</div>
 
 			<div class="col-lg-4 col-md-4">
 				<div class="fh5co-blog animate-box">
-					<a href="#"><img class="img-responsive" src="images/project-2.jpg" alt=""></a>
+					<a href="#"><img class="img-responsive" src="{{ asset('master/images/stock/tutor2.jpeg') }}" alt=""></a>
 					<div class="blog-text">
-						<h3><a href=""#>45 Minimal Worksspace Rooms for Web Savvys</a></h3>
-						<span class="posted_on">Nov. 15th</span>
-						<span class="comment"><a href="">21<i class="icon-speech-bubble"></i></a></span>
-						<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-						<a href="#" class="btn btn-primary">Read More</a>
+						<h3><a href=""#>Janet Doe</a></h3>
+						<span class="posted_on">Tutor since Nov. 15th, 2018</span>
+						<p class="comment">Tutorials Done: <a class="pull-right" href="">21<i class="icon-speech-bubble"></i></a></p>
+						<p>Locations Available: Lekki, VGC, Ajah</p>
+						<a href="#" class="btn btn-primary">Request</a>
 					</div> 
 				</div>
 			</div>
-
-			<div class="col-lg-4 col-md-4">
-				<div class="fh5co-blog animate-box">
-					<a href="#"><img class="img-responsive" src="images/project-3.jpg" alt=""></a>
-					<div class="blog-text">
-						<h3><a href=""#>45 Minimal Workspace Rooms for Web Savvys</a></h3>
-						<span class="posted_on">Nov. 15th</span>
-						<span class="comment"><a href="">21<i class="icon-speech-bubble"></i></a></span>
-						<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-						<a href="#" class="btn btn-primary">Read More</a>
-					</div> 
-				</div>
-			</div>
-
-			<div class="col-lg-4 col-md-4">
-				<div class="fh5co-blog animate-box">
-					<a href="#"><img class="img-responsive" src="images/project-3.jpg" alt=""></a>
-					<div class="blog-text">
-						<h3><a href=""#>45 Minimal Workspace Rooms for Web Savvys</a></h3>
-						<span class="posted_on">Nov. 15th</span>
-						<span class="comment"><a href="">21<i class="icon-speech-bubble"></i></a></span>
-						<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-						<a href="#" class="btn btn-primary">Read More</a>
-					</div> 
-				</div>
-			</div>
-
 		</div>
 	</div>
 </div>
