9d08c0ad0df9ed7b8f40ee207d78b28e96c888c0
Fanan Dala <fanan.dala@yahoo.com>
2019-04-27 13:56:51 +0100

    fixed register address and phone not adding

--- app/User.php
+++ app/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'phone', 'address'
     ];
     /* All the fields present on this array will be automatically accessible in your views with Carbon functions
     *
